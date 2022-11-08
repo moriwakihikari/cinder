4e1b7c0f6a65f6aae6c9941b1ba3153c6fd105ef
cin-moriwaki <moriwaki-h@cin-group.com>
2022-11-09 01:24:29 +0900

    MUI,gin追加,ログインページ表示

--- api/app/sql.go
+++ api/app/sql.go
@@ -9,7 +9,7 @@ import (
 )
 
 
-func main() {
+func main2() {
 	con, er := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(db:3306)/%s?charset=utf8&parseTime=true",
 		os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"),
 		os.Getenv("MYSQL_DATABASE")))
